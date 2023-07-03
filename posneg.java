a19e9f78afe7ae8784e465ce7548e105c8082250
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2023-07-03 23:08:56 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class posneg
+{
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the numbetr: ");
+
+int num=sc.nextInt();
+if (num>0)
+//int num=10;
+
+{
+	System.out.println("It is positibe");
+
+ 
+}
+
+else if(num<0)
+{
+	System.out.println("It is negative");
+	
+}
+else{
+	System.out.println("System.out.println");
+}
+
+
+}
+}
+		
+	
+	
