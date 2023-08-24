dc935acd3d2e5cec8dc43693ef98e12b043c0a17
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2023-08-24 15:06:20 +0530

    Add files via upload

---
+++ factors.java
@@ -0,0 +1,20 @@
+class factors
+{
+	public static void main(String[] args)
+	{
+		int num=6;
+		int count =0;
+		
+		for(int i=1; i<=num; i++)
+		{
+			if(num%i==0)
+			{
+				System.out.println(i);
+				
+				count++;
+				
+			}
+		}
+		System.out.println("the no of given factors: "+count);
+	}
+}
