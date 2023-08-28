40c7922d57babeaa85c18c55102fd1412e238fc3
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2023-08-28 17:40:31 +0530

    Add files via upload

---
+++ Sum_of_natural_num.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Sum_of_natural_num
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number: ");
+		
+		int num= sc.nextInt();
+		int sum=0;
+		
+		for(int i=1; i<=num; i++)
+		{
+			sum= sum+i;
+		}
+		System.out.println(sum);
+	}
+}
+/*
+Enter the number:
+6
+21
+
+*/
