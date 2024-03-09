907ce2bd58711e1729d64fdcfea989935d087f2d
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2024-03-09 22:59:25 +0300

    Add files via upload

---
+++ Method_program3.java
@@ -0,0 +1,15 @@
+class Method_program3 {
+    public static void main(String[] args) {
+        int a = 3;
+
+        System.out.println(a);
+
+        abc();
+    }
+
+    public static void abc() {
+        String a = "Qpider";
+
+        System.out.println(a);
+    }
+}
