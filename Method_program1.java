caf2fa158d157d68b971716e27cfdadf9e112fee
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2024-03-09 22:58:35 +0300

    Add files via upload

---
+++ Method_program1.java
@@ -0,0 +1,19 @@
+class Method_program1 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Main begin...");
+
+        c();
+        c();
+        c();
+
+        System.out.println("main end!");
+    }
+
+    public static void c()
+    {
+        System.out.println("From c() method.");
+    }
+
+}
