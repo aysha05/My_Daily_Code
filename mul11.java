fba68dcf87840cf59642d802ca76920ae22d79e2
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2024-03-09 21:32:57 +0300

    Add files via upload

---
+++ mul11.java
@@ -0,0 +1,12 @@
+class mul11 {
+    public static void main(String[] args) {
+        mul(4, 8);
+
+    }
+
+    public static void mul(int a, int b) {
+        int res = a * b;
+
+        System.out.println(res);
+    }
+}
