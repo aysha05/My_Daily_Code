77823f64d39034e0a1001f92905a52ab4b0d75fb
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2024-03-20 13:06:07 +0300

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,23 @@
+class Switch {
+    public static void main(String[] args) {
+
+        switch (2) {
+            case 1: {
+                System.out.println("from case 1");
+                break;
+            }
+            case 2: {
+                System.out.println("From case 2");
+                break;
+            }
+            case 3: {
+                System.out.println("From case 3");
+                break;
+            }
+            default: {
+                System.out.println("From default");
+            }
+
+        }
+    }
+}
