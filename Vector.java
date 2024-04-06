cd4b304ccec2e70cc6c3d936f6ef2baf87448554
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2024-04-06 21:29:21 +0300

    Add files via upload

---
+++ Vector.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+class Vector {
+
+    public static void main(String[] args) {
+
+        Vector v = new Vector();
+
+        v.addElement("Deepak");
+        v.add(1, "Rahul");
+        v.add("Aishwarya");
+
+        System.out.println(v);
+    }
+}
