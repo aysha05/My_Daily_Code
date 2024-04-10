4aac5305914e87efdda0fa7553ef8c2754d9fa7f
Aishwarya Kale <93932993+aysha05@users.noreply.github.com>
2024-04-10 03:05:45 +0300

    Add files via upload

---
+++ Vectorlegacydemo.java
@@ -0,0 +1,32 @@
+import java.util.Vector;
+
+class Vectorlegacydemo {
+
+    public static void main(String[] args) {
+
+        Vector v = new Vector();
+
+        v.addElement("Deepak");
+        v.add(1, "Rahul");
+        v.add(100);
+
+        // System.out.println(v);
+
+        // System.out.println(v.firstElement());
+        // System.out.println(v.lastElement());
+        // System.out.println(v);
+
+        // System.out.println(v.removeElement("Deepak"));
+        // System.out.println(v);
+
+        // v.removeElementAt(1);
+        // System.out.println(v);
+
+        // v.removeAllElements();
+        // System.out.println(v);
+
+        // v.capacity();
+        // System.out.println(v.capacity());
+
+    }
+}
